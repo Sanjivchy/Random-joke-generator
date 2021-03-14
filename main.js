cf92c3bbab9691ff367a52394f0da1929e5b524d
xoonziip <sanjivchaudhary416@gmail.com>
2021-03-14 12:50:45 +0545

    created puchline

--- main.js
+++ main.js
@@ -2,14 +2,19 @@ const setupDiv = document.getElementById('setup');
 const punchlineDiv = document.getElementById('punchline');
 const punchlineBtn = document.getElementById('punchlineBtn');
 const newJokeBtn = document.getElementById('newJokeBtn');
-
+let punchline;
 async function getjokes(){
     const jokePromise =await fetch('https://official-joke-api.appspot.com/jokes/programming/random')
     const joke =await jokePromise.json();
-    console.log(joke);
+    //console.log(joke);
     //get the setup from the joke and insert it into the setupdiv element
     setupDiv.innerHTML=joke[0].setup;
-    punchlineDiv.
+    //create a global variable which will stire the current punch line updated each time
+    // assginn the current punch line jokes punchline to the punchline variable
+    punchline=joke[0].punchline
+    //console.log(punchline);
+    
+
 }
 
 getjokes();
